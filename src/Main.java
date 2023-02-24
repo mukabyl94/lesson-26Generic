b146b8816369834f5a40eed244681bbc936f9748
mukabyl <mukabyl@yamdex.ru>
2023-02-24 17:14:44 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    method(new Integer[]{41,45,2,14,59,52,7,36});
+    }
+    public static <A> void method(A[] massiv){
+
+        System.out.println(massiv[0]);
+//        Integer a = (Integer) massiv[massiv.length-1];
+//        System.out.println(a);
+        System.out.println(massiv[massiv.length-1]);
+        System.out.println(Arrays.toString(massiv));
     }
 }
